66e8522a81ab03cfa8701468dbc10b8278467f97
Diogo_Ferreira <diogo.miguel.ferreira@tecnico.ulisboa.pt>
2023-09-20 11:49:37 +0100

    rmvd wrong files

--- Space_CoBot/SpaceCobot-ros_px4/px4-ROS/ros_controllers/space_cobot_ws/src/space_cobot_controller/cmake-build-debug/CMakeFiles/space_cobot_controller_generate_messages_cpp.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/space_cobot_controller_generate_messages_cpp"
-  "devel/include/space_cobot_controller/pwm_values.h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/space_cobot_controller_generate_messages_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- Space_CoBot/SpaceCobot-ros_px4/px4-ROS/ros_controllers/space_cobot_ws/src/space_cobot_controller/cmake-build-debug/catkin_generated/space_cobot_controller-msg-extras.cmake.develspace.in
+++
@@ -1,2 +0,0 @@
-set(space_cobot_controller_MESSAGE_FILES "/home/socrob/space_cobot_ws/src/space_cobot_controller/msg/pwm_values.msg")
-set(space_cobot_controller_SERVICE_FILES "")
