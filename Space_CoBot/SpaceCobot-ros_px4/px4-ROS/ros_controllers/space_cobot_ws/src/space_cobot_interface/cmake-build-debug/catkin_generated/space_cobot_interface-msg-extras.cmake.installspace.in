51bc0a6aaf3033f6b86c2d28952c641327c8e13c
Diogo_Ferreira <diogo.miguel.ferreira@tecnico.ulisboa.pt>
2023-09-20 11:49:37 +0100

    rmvd wrong files

--- Space_CoBot/SpaceCobot-ros_px4/px4-ROS/ros_controllers/space_cobot_ws/src/space_cobot_interface/cmake-build-debug/catkin_generated/space_cobot_interface-msg-extras.cmake.installspace.in
+++
@@ -1,2 +0,0 @@
-set(space_cobot_interface_MESSAGE_FILES "msg/pwm_values.msg")
-set(space_cobot_interface_SERVICE_FILES "")
--- Space_CoBot/SpaceCobot-ros_px4/px4-ROS/ros_controllers/space_cobot_ws/src/space_cobot_mpc_controller/cmake-build-debug/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/usr/local/include".split(';') if "/usr/local/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;rospy;std_msgs;message_generation;message_runtime;tf".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lspace_cobot_mpc_controller".split(';') if "-lspace_cobot_mpc_controller" != "" else []
-PROJECT_NAME = "space_cobot_mpc_controller"
-PROJECT_SPACE_DIR = "/usr/local"
-PROJECT_VERSION = "0.0.0"
