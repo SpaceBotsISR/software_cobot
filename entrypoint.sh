51a75c9b7a893747c34e9899b119ffd880b2180b
spaceCobot <scobot12@gmail.com>
2023-09-25 17:58:41 +0100

    first node test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-
 set -e
 
 source /opt/ros/humble/setup.bash
+source /usr/share/colcon_cd/function/colcon_cd.sh
+source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
+export _colcon_cd_root=/opt/ros/humble/
 
 echo "Provided arguments: $@"
 exec $@
