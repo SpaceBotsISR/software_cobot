537c93131ef932911ee6ad9cc2d96c412ebf6ac3
Diogo_Ferreira <diogo.miguel.ferreira@tecnico.ulisboa.pt>
2023-09-26 11:27:09 +0100

    feat(cobot): ros2 node

--- ros2_ws/src/test_pkg/src/number_publisher.cpp
+++ ros2_ws/src/test_pkg/src/number_publisher.cpp
@@ -1,11 +1,29 @@
 #include "rclcpp/rclcpp.hpp"
 
-int main(int argc, char * argv[]) {
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<rclcpp::Node>("number_publisher");
-    RCLCPP_INFO(node->get_logger(), "Hello World");
+class NumberPublisherNode : public rclcpp::Node {
+   public:
+    NumberPublisherNode() : Node("number_publisher"), number(0) {
+        number_timer = this->create_wall_timer(std::chrono::seconds(1),
+                                               std::bind(&NumberPublisherNode::print_number, this));
 
+        RCLCPP_INFO(this->get_logger(), "Node Started");
+    }
+
+   private:
+    int number;
+    rclcpp::TimerBase::SharedPtr number_timer;
+
+    void print_number() {
+        RCLCPP_INFO(this->get_logger(), "Number: %d", number);
+        number++;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<NumberPublisherNode>();
     rclcpp::spin(node);
     rclcpp::shutdown();
+
     return 0;
 }
