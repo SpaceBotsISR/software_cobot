f74399d9fed134f98c9f00ed1928abc1e8ecc38d
spaceCobot <scobot12@gmail.com>
2023-09-26 16:04:33 +0100

    updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ source /opt/ros/humble/setup.bash
 source /usr/share/colcon_cd/function/colcon_cd.sh
 source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
 export _colcon_cd_root=/opt/ros/humble/
+export QT_QPA_PLATFORM=xcb
 
 echo "Provided arguments: $@"
 exec $@
