1a32456d7006d66f487c880e701752e144846a70
Diogo_Ferreira <diogo.miguel.ferreira@tecnico.ulisboa.pt>
2023-10-02 16:05:46 +0100

    Changed subscriber to class

--- scripts/camera_streaming/subscriber/subscriber.cpp
+++ scripts/camera_streaming/subscriber/subscriber.cpp
@@ -12,73 +12,102 @@ const int display_height = 540;
 const int framerate = 30;
 const int flip_method = 0;
 
-int main() {
-    int server_socket, client_socket;
-    struct sockaddr_in server_addr;
-    char buffer[display_width * display_height * 3];  // Assuming 3 channels (BGR)
-    char len_buffer[48] = {0};
+class VideoClient {
+   public:
+    VideoClient(int port) {
+        // Create socket
+        server_socket = socket(AF_INET, SOCK_STREAM, 0);
+        if (server_socket < 0) {
+            std::cerr << "Error creating socket!" << std::endl;
+            exit(1);
+        }
 
-    // Create socket
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket < 0) {
-        std::cerr << "Error creating socket!" << std::endl;
-        return -1;
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = htons(port);
+        server_addr.sin_addr.s_addr = INADDR_ANY;
+
+        // Bind
+        bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr));
+
+        // Listen
+        listen(server_socket, 1);
+        std::cout << "Listening for incoming connections..." << std::endl;
+
+        // Accept client connection
+        client_socket = accept(server_socket, NULL, NULL);
+        std::cout << "Connected!" << std::endl;
     }
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(8080);
-    server_addr.sin_addr.s_addr = INADDR_ANY;
+    void run() {
+        cv::namedWindow("Received Frames", cv::WINDOW_AUTOSIZE);
 
-    // Bind
-    bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
+        while (true) {
+            ssize_t msg_size = get_msg_size();
+            ssize_t total_bytes_received = 0;
+            ssize_t offset = 0;
 
-    // Listen
-    listen(server_socket, 1);
-    std::cout << "Listening for incoming connections..." << std::endl;
+            read_and_display(msg_size);
 
-    // Accept client connection
-    client_socket = accept(server_socket, NULL, NULL);
-    std::cout << "Connected!" << std::endl;
+            int keyCode = cv::waitKey(10) & 0xFF;
+            if (keyCode == 27 || keyCode == 'q') {
+                break;
+            }
+        }
+    }
 
-    cv::namedWindow("Received Frames", cv::WINDOW_AUTOSIZE);
+    ~VideoClient() {
+        close(client_socket);
+        close(server_socket);
+    }
 
-    while (true) {
-        cv::Mat frame(display_height, display_width, CV_8UC3);
+   private:
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr;
+
+    ssize_t get_msg_size() {
+        char len_buffer[48] = {0};
 
         ssize_t bytes_received = recv(client_socket, len_buffer, sizeof(len_buffer), 0);
         if (bytes_received <= 0) {
             std::cerr << "Connection closed or error" << std::endl;
-            break;
+            return;
         }
 
         std::cout << "\n\nlen_buffer: " << len_buffer << std::endl;
-        int msg_size = std::stoi(len_buffer);
-        ssize_t total_bytes_recieved = 0;
-        ssize_t offset = 0; 
+        return std::stol(len_buffer);
+    }
 
-        while (total_bytes_recieved < msg_size) {
-            bytes_received = recv(client_socket, buffer + offset, msg_size - total_bytes_recieved, 0);
+    void read_and_display(ssize_t msg_size) {
+        cv::Mat frame(display_height, display_width, CV_8UC3);
+        std::unique_ptr<char[]> buffer = std::make_unique<char[]>(msg_size);
+
+        ssize_t total_bytes_received = 0;
+        ssize_t offset = 0;
+        ssize_t bytes_received;
+
+        while (total_bytes_received < msg_size) {
+            bytes_received =
+                recv(client_socket, buffer + offset, msg_size - total_bytes_received, 0);
 
             if (bytes_received <= 0) {
                 std::cerr << "Connection closed or error" << std::endl;
-                break;
+                exit(1);
             }
 
-            total_bytes_recieved += bytes_received;
+            total_bytes_received += bytes_received;
             offset += bytes_received;  // Update the offset by the number of bytes received
-            std::cout << "Recived: " << bytes_received << " | Total: " << total_bytes_recieved << std::endl;
+            std::cout << "Recived: " << bytes_received << " | Total: " << total_bytes_received
+                      << std::endl;
         }
 
-        std::memcpy(frame.data, buffer, total_bytes_recieved);
+        std::memcpy(frame.data, buffer, total_bytes_received);
         cv::imshow("Received Frames", frame);
-
-        int keyCode = cv::waitKey(10) & 0xFF;
-        if (keyCode == 27 || keyCode == 'q') {
-            break;
-        }
     }
+};
 
-    close(client_socket);
-    close(server_socket);
+int main() {
+    VideoClient client(8080);
+    client.run();
+
     return 0;
 }
