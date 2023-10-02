95b5896e0e29928d353f61c068e918587adb7e2e
Diogo_Ferreira <diogo.miguel.ferreira@tecnico.ulisboa.pt>
2023-10-02 14:52:10 +0100

    sending and recieving on a loop

--- scripts/camera_streaming/subscriber/subscriber.cpp
+++ scripts/camera_streaming/subscriber/subscriber.cpp
@@ -1,10 +1,16 @@
-#include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
-#include <iostream>
 #include <opencv2/opencv.hpp>
+
+#define CHECK_SOCKET(B)                                         \
+    if (B <= 0) {                                               \
+        std::cerr << "Connection closed or error" << std::endl; \
+        close(client_socket);                                   \
+        close(server_socket);                                   \
+        exit(1);                                                \
+    }
 
 const int sensor_id = 0;
 const int capture_width = 1920;
@@ -14,83 +20,62 @@ const int display_height = 540;
 const int framerate = 30;
 const int flip_method = 0;
 
-class VideoStreamClient {
-   public:
-    VideoStreamClient(std::string ip_address, int port) : isConnected(false) {
-        client_socket = socket(AF_INET, SOCK_STREAM, 0);
-        if (client_socket < 0) {
-            std::cerr << "Error creating socket!" << std::endl;
-            return;
-        }
-
-        server_addr.sin_family = AF_INET;
-        server_addr.sin_port = htons(port);
-        server_addr.sin_addr.s_addr = inet_addr(ip_address.c_str());
-
-        if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-            std::cerr << "Error connecting to server!" << std::endl;
-            return;
-        }
-
-        isConnected = true;
-    }
-
-    void receiveAndDisplay() {
-        if (!isConnected) {
-            std::cerr << "Not connected to server!" << std::endl;
-            return;
-        }
-
-        cv::namedWindow("Video Stream", cv::WINDOW_AUTOSIZE);
-
-        while (isConnected) {
-            cv::Mat frame = receive_frame();
-            if (frame.empty()) {
-                std::cerr << "Received an empty frame or error occurred!" << std::endl;
-                break;
-            }
-
-            cv::imshow("Video Stream", frame);
-            if (cv::waitKey(1) >= 0) break;  // Exit on any key press
-        }
-    }
-
-    ~VideoStreamClient() {
-        if (isConnected) {
-            close(client_socket);
-        }
-    }
-
-   private:
-    int client_socket;
-    struct sockaddr_in server_addr;
-    bool isConnected;
-
-    cv::Mat receive_frame() {
-        int imgSize = 1920 * 1080 * 3;  // Assuming 3 bytes per pixel (BGR format)
-        std::vector<uchar> img_data(imgSize);
-
-        ssize_t bytesReceived = 0;
-        uchar* buffer_ptr = img_data.data();
-
-        while (bytesReceived < imgSize) {
-            ssize_t bytesRead = recv(client_socket, buffer_ptr, imgSize - bytesReceived, 0);
-            if (bytesRead <= 0) {
-                std::cerr << "Error receiving data or connection closed by server!" << std::endl;
-                isConnected = false;
-                return cv::Mat();
-            }
-            bytesReceived += bytesRead;
-            buffer_ptr += bytesRead;
-        }
-
-        return cv::Mat(capture_height, capture_width, CV_8UC3, img_data.data());
-    }
-};
-
 int main() {
-    VideoStreamClient client("127.0.0.1", 8080);
-    client.receiveAndDisplay();
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr;
+    char buffer[display_width * display_height * 3];  // Assuming 3 channels (BGR)
+    char len_buffer[48];
+
+    // Create socket
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0) {
+        std::cerr << "Error creating socket!" << std::endl;
+        return -1;
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    // Bind
+    bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
+
+    // Listen
+    listen(server_socket, 1);
+    std::cout << "Listening for incoming connections..." << std::endl;
+
+    // Accept client connection
+    client_socket = accept(server_socket, NULL, NULL);
+    std::cout << "Connected!" << std::endl;
+
+    cv::namedWindow("Received Frames", cv::WINDOW_AUTOSIZE);
+
+    while (true) {
+        cv::Mat frame(display_height, display_width, CV_8UC3);
+
+        ssize_t bytes_received = recv(client_socket, len_buffer, sizeof(len_buffer), 0);
+        CHECK_SOCKET(bytes_received);
+
+        ssize_t msg_size = std::stoi(len_buffer);
+        ssize_t total_bytes_recieved = 0;
+
+        while (total_bytes_recieved < msg_size) {
+            bytes_received =
+                recv(client_socket, buffer + bytes_received, msg_size - bytes_received, 0);
+
+            CHECK_SOCKET(bytes_received);
+
+            bytes_received += bytes_received;
+        }
+
+        std::memcpy(frame.data, buffer, bytes_received);
+        cv::imshow("Received Frames", frame);
+
+        int keyCode = cv::waitKey(10) & 0xFF;
+        if (keyCode == 27 || keyCode == 'q') {
+            break;
+        }
+    }
 
     return 0;
 }
