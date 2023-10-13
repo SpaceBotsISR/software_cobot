274aeab2b0395e91352a9da6bf45f1e42801daf4
andre-rebelo-teixeira <andre.r.teixeira@tecnico.ulisboa.pt>
2023-10-13 13:26:51 +0000

    feat(ros1_repo): pkg removed from git
    Since space_cobot_controller and mocap_interface are now implemente in ros2 they are being removed from repo and will stay only on older commits

--- space_cobot_ws/src/space_cobot_controller/cmake-build-debug/CMakeFiles/space_cobot_controller_generate_messages_lisp.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/space_cobot_controller_generate_messages_lisp"
-  "devel/share/common-lisp/ros/space_cobot_controller/msg/pwm_values.lisp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/space_cobot_controller_generate_messages_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- space_cobot_ws/src/space_cobot_controller/cmake-build-debug/CMakeFiles/space_cobot_controller_genlisp.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/space_cobot_controller_genlisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
