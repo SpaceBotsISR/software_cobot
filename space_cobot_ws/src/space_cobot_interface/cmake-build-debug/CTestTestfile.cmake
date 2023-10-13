33dabe31b10493138aefa493d84e0a8866df2d7e
andre-rebelo-teixeira <andre.r.teixeira@tecnico.ulisboa.pt>
2023-10-13 13:24:08 +0000

    feat(ros1_repo): since package is now in ros2 i remove ros2 version and will mantain only on git

--- space_cobot_ws/src/space_cobot_interface/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/socrob/space_cobot_ws/src/space_cobot_interface
-# Build directory: /home/socrob/space_cobot_ws/src/space_cobot_interface/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
