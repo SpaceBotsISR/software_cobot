5533b906cd4da21377b6213fb2306a71f386e012
andre-rebelo-teixeira <andre.r.teixeira@tecnico.ulisboa.pt>
2023-10-13 13:24:08 +0000

    feat(ros1_repo): since package is now in ros2 i remove ros2 version and will mantain only on git

--- space_cobot_ws/src/space_cobot_interface/cmake-build-debug/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/socrob/space_cobot_ws/src/space_cobot_interface/cmake-build-debug/devel/include;/home/socrob/space_cobot_ws/src/space_cobot_interface/include".split(';') if "/home/socrob/space_cobot_ws/src/space_cobot_interface/cmake-build-debug/devel/include;/home/socrob/space_cobot_ws/src/space_cobot_interface/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "geometry_msgs;message_generation;message_runtime;roscpp;rospy;sensor_msgs;std_msgs;tf".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lspace_cobot_interface".split(';') if "-lspace_cobot_interface" != "" else []
-PROJECT_NAME = "space_cobot_interface"
-PROJECT_SPACE_DIR = "/home/socrob/space_cobot_ws/src/space_cobot_interface/cmake-build-debug/devel"
-PROJECT_VERSION = "0.0.0"
