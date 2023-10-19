539107e0d39ce218a2caebec8315b02ae448036a
Diogo_Ferreira <diogo.miguel.ferreira@tecnico.ulisboa.pt>
2023-10-19 14:28:22 +0100

    camera streeming update

---
+++ camera_streamer/CMakeFiles/camera_streamer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/camera_streamer.dir/camera_streamer.cpp.o"
+  "CMakeFiles/camera_streamer.dir/camera_streamer.cpp.o.d"
+  "camera_streamer"
+  "camera_streamer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/camera_streamer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
