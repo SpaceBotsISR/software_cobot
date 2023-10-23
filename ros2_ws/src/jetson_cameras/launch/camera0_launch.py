1b36832bcbee01cc2c66dc5811f2a10e6c91b65c
Diogo_Ferreira <diogo.miguel.ferreira@tecnico.ulisboa.pt>
2023-10-23 18:44:28 +0100

    Launch file for each camera + node cahnges for multicamera support

---
+++ ros2_ws/src/jetson_cameras/launch/camera0_launch.py
@@ -0,0 +1,16 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            Node(
+                package="jetson_cameras",
+                executable="camera_publisher",
+                name="camera_publisher0",
+                output="screen",
+                parameters=[{"camera_id": 0}],
+            )
+        ]
+    )
