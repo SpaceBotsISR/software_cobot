61b3b95514cdc82cd2c9c3b46c50f5e359a67829
Diogo Ferreira <itsnotsoftware@gmail.com>
2023-11-07 02:33:33 +0000

    visualization ready

--- ros2_ws/src/jetson_cameras/launch/camera0_launch.py
+++ ros2_ws/src/jetson_cameras/launch/camera0_launch.py
@@ -1,46 +1,28 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription, Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
 
 ENABLE_ARUCO = True
 
+
 def generate_launch_description():
-    jetson_cameras_dir = get_package_share_directory('jetson_cameras')
-    camera_params = jetson_cameras_dir + '/config/camera_params.yaml'
+    jetson_cameras_dir = get_package_share_directory("jetson_cameras")
+    camera_params = jetson_cameras_dir + "/config/camera_params.yaml"
 
     camera_node = Node(
-        package='jetson_cameras',
-        executable='camera_publisher',
-        name='camera_publisher0',
-        #output='screen',
-        parameters=[{'camera_id': 0}, camera_params],
+        package="jetson_cameras",
+        executable="camera_publisher",
+        name="camera_publisher0",
+        parameters=[{"camera_id": 0}, camera_params],
     )
 
-    aruco_node = Node(
-        package='ros2_aruco',
-        executable='aruco_node',
-        name='aruco_node0',
-        output='screen',
-        parameters=[{
-            'marker_size': 0.052,
-            'aruco_dictionary_id': 'DICT_4X4_250',
-            'image_topic': '/camera_0/image_raw', 
-            'camera_info_topic': '/camera_0/camera_info'
-        }],
-        # remappings=[
-        #     ('/image_raw', '/camera_0/image_raw'),
-        #     ('/camera_info', '/camera_0/image_raw'),
-        # ]
+    aruco_node = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(jetson_cameras_dir + "/launch/aruco0_launch.py"),
     )
 
     if ENABLE_ARUCO:
-        return LaunchDescription([
-                camera_node,
-                aruco_node
-        ])
+        return LaunchDescription([camera_node, aruco_node])
     else:
-        return LaunchDescription([
-            camera_node
-        ])
-
+        return LaunchDescription([camera_node])
