7b689321120e6458843a98ffbe10d437b57b7e29
ItsNotSoftware <itsnotsoftware@gmail.com>
2023-11-07 18:39:10 +0000

    multiple aruco detect but crashing

--- ros2_ws/src/jetson_cameras/launch/camera0_launch.py
+++ ros2_ws/src/jetson_cameras/launch/camera0_launch.py
@@ -1,5 +1,6 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, Node
+from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
