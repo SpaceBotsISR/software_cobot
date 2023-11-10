1a03143f244b5aeba340bc9d3ca355a34e971b1d
ItsNotSoftware <itsnotsoftware@gmail.com>
2023-11-10 01:39:31 +0000

    new aruco detection c++

---
+++ ros2_ws/src/aruco_opencv/launch/aruco1.launch.py
@@ -0,0 +1,20 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    config = os.path.join(get_package_share_directory('aruco_opencv'), 'config', 'aruco_tracker.yaml')
+    board_descriptions = os.path.join(get_package_share_directory('aruco_opencv'), 'config', 'board_descriptions.yaml')
+    
+    return LaunchDescription([
+        Node(
+            package='aruco_opencv',
+            executable='aruco_tracker_autostart',
+            name='aruco_node1',
+            parameters=[config, {'cam_base_topic': 'camera_1/image_raw'}, {'output_frame': 'camera_frame_1'}, {'board_descriptions_path': ''}],
+            output='screen'
+        ),
+    ])
