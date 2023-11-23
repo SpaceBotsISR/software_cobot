fec1e0fc33e860fd360b4f31d197614034b70cbf
Diogo Ferreira <itsnotsoftware@gmail.com>
2023-11-23 01:14:24 +0000

    added directions

--- vSLAM_dev_sandbox/2d/pyvis.py
+++ vSLAM_dev_sandbox/2d/pyvis.py
@@ -20,25 +20,53 @@ class PyVis:
         self.fig, self.ax = plt.subplots()
         self.ax.set_xlim(0, 40)
         self.ax.set_ylim(0, 40)
-        self.ax.scatter(*zip(*self.landmarks), marker="*", color="b", label="Landmarks")
+        self.ax.scatter(
+            *zip(*self.landmarks), marker="*", color="b", alpha=0.6, label="Landmarks"
+        )
 
         for i, (x, y) in enumerate(self.landmarks):
             self.ax.text(
                 x, y, "id = " + str(i), fontsize=8, ha="center", va="bottom", color="b"
             )
 
-        self.ax.grid()
         self.ax.legend()
+
+    def draw_direction_line(self, x: float, y: float, theta: float, color: str):
+        # Length of the direction line
+        line_length = 2
+
+        # Calculate the endpoint of the direction line
+        end_x = x + line_length * np.cos(theta)
+        end_y = y + line_length * np.sin(theta)
+
+        # Draw the line
+        self.ax.plot([x, end_x], [y, end_y], color=color)
 
     def add_odom_point(self, x: float, y: float, theta: float) -> None:
         self.odom_points.append((x, y))
-        self.ax.scatter(x, y, marker="o", color="g", label="Odom Points")
+        self.ax.scatter(
+            x,
+            y,
+            marker="o",
+            color="g",
+            label="Odom Points" if len(self.odom_points) == 1 else "",
+        )
+        self.draw_direction_line(x, y, theta, color="g")
         self.ax.legend()
         self.fig.canvas.flush_events()  # Update the plot
 
     def add_ground_truth_point(self, x: float, y: float, theta: float) -> None:
         self.ground_truth_points.append((x, y))
-        self.ax.scatter(x, y, marker="o", color="r", label="Ground Truth Points")
+        self.ax.scatter(
+            x,
+            y,
+            marker="o",
+            color="r",
+            label="Ground Truth Points" if len(self.ground_truth_points) == 1 else "",
+        )
+
+        # Draw the direction line for ground truth point
+        self.draw_direction_line(x, y, theta, color="r")
 
         # Calculate FoV line endpoints
         left_fov_x, left_fov_y = self.calculate_fov_endpoint(
@@ -60,7 +88,7 @@ class PyVis:
         self.fig.canvas.flush_events()  # Update the plot
 
     def calculate_fov_endpoint(
-        self, x: float, y: float, theta: float, direction: float = "left"
+        self, x: float, y: float, theta: float, direction: str = "left"
     ) -> tuple[float, float]:
         # Calculate the half angle offset based on the direction
         angle_offset = self.fov_angle / 2
@@ -68,7 +96,6 @@ class PyVis:
             angle_offset = -angle_offset
 
         # Calculate the absolute angle considering the robot's orientation
-        # Assuming the robot is facing upwards (90 degrees)
         absolute_angle = theta + angle_offset
 
         # Calculate the endpoint coordinates
@@ -79,7 +106,14 @@ class PyVis:
 
     def add_slam_point(self, x: float, y: float, theta: float) -> None:
         self.slam_points.append((x, y))
-        self.ax.scatter(x, y, marker="o", color="m", label="SLAM Points")
+        self.ax.scatter(
+            x,
+            y,
+            marker="o",
+            color="m",
+            label="SLAM Points" if len(self.slam_points) == 1 else "",
+        )
+        self.draw_direction_line(x, y, theta, color="m")
         self.ax.legend()
         self.fig.canvas.flush_events()  # Update the plot
 
