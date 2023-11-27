630335af4df46b0893cd3e0e622f33dca44e6e56
Diogo Ferreira <itsnotsoftware@gmail.com>
2023-11-27 14:42:57 +0000

    sockets working fine

--- vSLAM_dev_sandbox/2d/src/main.cpp
+++ vSLAM_dev_sandbox/2d/src/main.cpp
@@ -1,18 +1,29 @@
+#include <chrono>
+#include <cmath>
 #include <iostream>
+#include <thread>
 
 #include "client.hpp"
 
-int main() {
+void test_client() {
     Client client("127.0.0.1", 8009);
+    std::vector<double> pose = {0.0, 0.0, 0.0};
 
     while (true) {
-        std::vector<float> msg = client.receive_msg();
-        std::cout << "Received message: ";
-        for (auto& i : msg) {
-            std::cout << i << " ";
-        }
-        std::cout << std::endl;
-    }
+        std::vector<double> msg = client.receive_msg();
+        double vx = msg[0];
+        double vy = msg[1];
+        double v_theta = msg[2];
 
+        pose[2] += v_theta;
+        pose[0] += vx * cos(pose[2]) + vy * sin(pose[2]);
+        pose[1] += vx * sin(pose[2]) + vy * cos(pose[2]);
+
+        client.send_msg(pose);
+    }
+}
+
+int main() {
+    test_client();
     return 0;
 }
