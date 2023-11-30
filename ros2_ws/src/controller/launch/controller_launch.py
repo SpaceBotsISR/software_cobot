cc08ae79139fe4b691c7ccca6dab7fda7eccb974
andre-rebelo-teixeira <andre.r.teixeira@tecnico.ulisboa.pt>
2023-11-30 10:54:26 +0000

    refactor(controller): cmakefile package.xml and launch files

---
+++ ros2_ws/src/controller/launch/controller_launch.py
@@ -0,0 +1,46 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import GroupAction
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    mavros_dir =  get_package_share_directory('mavros')
+    
+    launch  =  os.path.join (
+        get_package_share_directory ('space_cobot_controller'),
+        'config',
+        'autox.yml'
+    )
+    
+    return LaunchDescription([
+        Node (
+            package='space_cobot_interface', 
+            namespace='Space_Cobot_basic',
+            name='space_cobot_interface', 
+            executable='spaceCobotInterface', 
+            output='screen', 
+        ), 
+        Node(package='controller', 
+             namespace='Controller', 
+             name='controller_master', 
+             executable), 
+        Node (
+            package='space_cobot_controller',
+            namespace='Space_Cobot_basic',
+            name='space_cobot_controller',
+            executable='spaceCobotController',
+            output='screen',
+        )#, 
+   #     IncludeLaunchDescription (
+   #         PythonLaunchDescriptionSource(mavros_dir + '/launch/px4.launch'), 
+   #     )
+    ])
+
+def main(): 
+    generate_launch_description()
+
+if __name__ == '__main__':
+    main()
