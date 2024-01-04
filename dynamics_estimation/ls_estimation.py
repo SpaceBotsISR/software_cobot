23d90b1ca6fe60546a303c8909f14a5790932078
Diogo Ferreira <itsnotsoftware@gmail.com>
2024-01-04 14:52:59 +0000

    solver appears to be woking

--- dynamics_estimation/ls_estimation.py
+++ dynamics_estimation/ls_estimation.py
@@ -29,13 +29,14 @@ with
     R_k,k+1 = USV (USV is the SVD decomposition of (R_k,k+1) and S = diag(1,1,det(U)det(V))
 """
 
-G = 9.81 # gravity acceleration
+G = 9.81  # gravity acceleration
 g = np.array([[0], [0], [-G]], dtype=float)
-m = 3 # mass of Cobot
-m_batt = 0.5 # mass of the battery
-M = m + m_batt # total mass
-r = 0.25 # radius of Cobot
-h = 0.10 # height of Cobot
+m = 3  # mass of Cobot
+m_batt = 0.5  # mass of the battery
+M = m + m_batt  # total mass
+r = 0.25  # radius of Cobot
+h = 0.10  # height of Cobot
+
 
 class Estimator:
     def __init__(self, file_name="input_data.txt"):
@@ -54,7 +55,7 @@ class Estimator:
             print(f"R:\n{self.R[i]}")
             print(f"u:\n{self.u[i]}")
 
-    def get_L(self, mass = M):
+    def get_L(self, mass=M):
         """
         Get lower triangular matrix 3x3 from the moment of inertia.
 
@@ -64,23 +65,18 @@ class Estimator:
         Output:
         - L: Lower triangular matrix 3x3 as list of values.
         """
-        Ixx = Iyy = (1/12) * mass * (3 * r**2 + h**2)
-        Izz = (1/2) * mass * r**2
+        Ixx = Iyy = (1 / 12) * mass * (3 * r**2 + h**2)
+        Izz = (1 / 2) * mass * r**2
 
         # Inertia matrix
-        J_C = np.array([
-            [Ixx, 0, 0],
-            [0, Iyy, 0],
-            [0, 0, Izz]
-        ])
+        J_C = np.array([[Ixx, 0, 0], [0, Iyy, 0], [0, 0, Izz]])
 
         J1 = J_C / M
 
         # Cholesky decomposition
         L = np.linalg.cholesky(J1)
 
-        return np.array([L[0,0], L[1,0], L[1,1], L[2,0], L[2,1] , L[2,2]])
-
+        return np.array([L[0, 0], L[1, 0], L[1, 1], L[2, 0], L[2, 1], L[2, 2]])
 
     """
     v 0 0
@@ -91,7 +87,7 @@ class Estimator:
     def get_R(self, line):
         """
         Convert a quaternion to a 3x3 rotation matrix.
-    
+
         Parameters:
         - quaternion: The quaternion in the format (w, x, y, z).
 
@@ -108,7 +104,7 @@ class Estimator:
         line = line.replace("\n", "").replace(" ", ",")
         numbers = line.strip().split(",")
 
-        return np.array([[float(n) for n in numbers if n != '']])
+        return np.array([[float(n) for n in numbers if n != ""]])
 
     def read_file(self, file_name):
         with open(file_name) as fp:
@@ -129,62 +125,116 @@ class Estimator:
                 | -v2     v1      0 |
         """
         v = v.flatten()
-        return np.array([[0, -v[2], v[1]], 
-                         [v[2], 0, -v[0]], 
-                         [-v[1], v[0], 0]])
+        return np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
 
     def cost_function(self, params):
         residuals = []
         L_flat, c, A1M_flat = np.split(params, [6, 9])
-        L = np.array([[0, 0, 0],
-                     [L_flat[0], L_flat[1], 0],
-                     [L_flat[3], L_flat[4], L_flat[5]]])
+        L = np.array(
+            [[0, 0, 0], [L_flat[0], L_flat[1], 0], [L_flat[3], L_flat[4], L_flat[5]]]
+        )
 
         A1M = A1M_flat.reshape((3, 6))
-
 
         for i in range(len(self.timestamp) - 1):
             w_diff = (self.w[i + 1] - self.w[i]) / self.timestamp[i]
             w_avg = (self.w[i + 1] + self.w[i]) / 2
             u_avg = (self.u[i + 1] + self.u[i]) / 2
             R_svd = np.linalg.svd(self.R[i + 1])
-            R_avg = R_svd[0] @ np.diag([1, 1, np.linalg.det(R_svd[0]) * np.linalg.det(R_svd[2])]) @ R_svd[2]
+            R_avg = (
+                R_svd[0]
+                @ np.diag([1, 1, np.linalg.det(R_svd[0]) * np.linalg.det(R_svd[2])])
+                @ R_svd[2]
+            )
 
-            model = (L @ L.T @ w_diff +
-                self.get_S(w_avg) @ L @ L.T @ w_avg -
-                self.get_S(c) @ R_avg.T @ g -
-                A1M @ u_avg)
-
+            model = (
+                L @ L.T @ w_diff
+                + self.get_S(w_avg) @ L @ L.T @ w_avg
+                - self.get_S(c) @ R_avg.T @ g
+                - A1M @ u_avg
+            )
 
             residuals.append(model.flatten())
 
         return np.concatenate(residuals)
-    
+
+
 def main():
     est = Estimator()
 
     # Initial guesses
     L = est.get_L()
     c = np.zeros((3, 1), dtype=float)
-    A = np.array([
-        [-6.776660281601378126e-17, -4.069248629204854639e-01, 2.905744659368497129e-01, -1.897464878848385925e-15, -3.334542096204833328e+00, -1.329837852169367229e+00],
-        [-3.524072687206405985e-01, 2.034624314602428152e-01, 2.905744659368497129e-01, 2.887798165301998399e+00, -1.667271048102417330e+00, 1.329837852169366563e+00],
-        [3.524072687206407650e-01, 2.034624314602425932e-01, 2.905744659368496019e-01, 2.887798165302000175e+00, 1.667271048102416220e+00, -1.329837852169366341e+00],
-        [-1.107312451872372199e-16, -4.069248629204853529e-01, 2.905744659368496574e-01, 4.429249807489488797e-16, 3.334542096204833328e+00, 1.329837852169366563e+00],
-        [-3.524072687206407095e-01, 2.034624314602426765e-01, 2.905744659368496019e-01, -2.887798165301998843e+00, 1.667271048102417108e+00, -1.329837852169367229e+00],
-        [3.524072687206407650e-01, 2.034624314602426765e-01, 2.905744659368496019e-01, -2.887798165301999287e+00, -1.667271048102416220e+00, 1.329837852169366785e+00]
-    ])
+    A = np.array(
+        [
+            [
+                -6.776660281601378126e-17,
+                -4.069248629204854639e-01,
+                2.905744659368497129e-01,
+                -1.897464878848385925e-15,
+                -3.334542096204833328e00,
+                -1.329837852169367229e00,
+            ],
+            [
+                -3.524072687206405985e-01,
+                2.034624314602428152e-01,
+                2.905744659368497129e-01,
+                2.887798165301998399e00,
+                -1.667271048102417330e00,
+                1.329837852169366563e00,
+            ],
+            [
+                3.524072687206407650e-01,
+                2.034624314602425932e-01,
+                2.905744659368496019e-01,
+                2.887798165302000175e00,
+                1.667271048102416220e00,
+                -1.329837852169366341e00,
+            ],
+            [
+                -1.107312451872372199e-16,
+                -4.069248629204853529e-01,
+                2.905744659368496574e-01,
+                4.429249807489488797e-16,
+                3.334542096204833328e00,
+                1.329837852169366563e00,
+            ],
+            [
+                -3.524072687206407095e-01,
+                2.034624314602426765e-01,
+                2.905744659368496019e-01,
+                -2.887798165301998843e00,
+                1.667271048102417108e00,
+                -1.329837852169367229e00,
+            ],
+            [
+                3.524072687206407650e-01,
+                2.034624314602426765e-01,
+                2.905744659368496019e-01,
+                -2.887798165301999287e00,
+                -1.667271048102416220e00,
+                1.329837852169366785e00,
+            ],
+        ]
+    )
     A1 = A / M
     A1M = A1[3:, :]
 
     initial_params_list = [L, c, A1M]
     initial_params = np.concatenate([param.flatten() for param in initial_params_list])
 
-    result = sopt.least_squares(est.cost_function, initial_params, verbose = 2)
+    result = sopt.least_squares(est.cost_function, initial_params, verbose=2)
 
-    optimized_L = result.x[:9].reshape((3, 3))
-    optimized_c = result.x[9:12].reshape((3, 1))
-    optimized_A1M = result.x[12:].reshape((3, 6))
+    optimized_L = result.x[:6]
+    optimized_L = np.array(
+        [
+            [optimized_L[0], 0, 0],
+            [optimized_L[1], optimized_L[2], 0],
+            [optimized_L[3], optimized_L[4], optimized_L[5]],
+        ]
+    )
+    optimized_c = result.x[6:9].reshape((3, 1))
+    optimized_A1M = result.x[9:].reshape((3, 6))
 
     print("Optimized L:")
     print(optimized_L)
