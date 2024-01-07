b504e620a5c45b10aceca97164b61056838f2f0e
Diogo Ferreira <itsnotsoftware@gmail.com>
2024-01-07 18:30:10 +0000

    pre calculating parameters + fixed L

--- dynamics_estimation/ls_estimation.py
+++ dynamics_estimation/ls_estimation.py
@@ -29,8 +29,8 @@ with
     R_k,k+1 = USV (USV is the SVD decomposition of (R_k,k+1) and S = diag(1,1,det(U)det(V))
 """
 
-G = 9.81  # gravity acceleration
-g = np.array([[0], [0], [-G]], dtype=float)
+# Cobot parameters
+g = np.array([[0], [0], [-9.81]], dtype=float)  # gravity acceleration
 m = 3  # mass of Cobot
 m_batt = 0.5  # mass of the battery
 M = m + m_batt  # total mass
@@ -46,6 +46,7 @@ class Estimator:
         self.u = []
 
         self.read_file(file_name)
+        self.peprocess_data()
 
     def print(self):
         for i in range(len(self.timestamp)):
@@ -55,7 +56,8 @@ class Estimator:
             print(f"R:\n{self.R[i]}")
             print(f"u:\n{self.u[i]}")
 
-    def get_L(self, mass=M):
+    @property
+    def L(self, mass=M):
         """
         Get lower triangular matrix 3x3 from the moment of inertia.
 
@@ -78,12 +80,6 @@ class Estimator:
 
         return np.array([L[0, 0], L[1, 0], L[1, 1], L[2, 0], L[2, 1], L[2, 2]])
 
-    """
-    v 0 0
-    v v 0
-    v v v
-    """
-
     def get_R(self, line):
         """
         Convert a quaternion to a 3x3 rotation matrix.
@@ -101,12 +97,124 @@ class Estimator:
         return R
 
     def parse_line(self, line):
+        """
+        Convert a line of numbers to a numpy array.
+
+        Parameters:
+        - line: The line of numbers.
+
+        Output:
+        - array: The array of numbers.
+        """
         line = line.replace("\n", "").replace(" ", ",")
         numbers = line.strip().split(",")
 
         return np.array([[float(n) for n in numbers if n != ""]])
 
+    def get_S(self, v):
+        """
+                |   0    -v3     v2 |
+        S(v) =  |  v3      0    -v1 |
+                | -v2     v1      0 |
+        """
+        v = v.flatten()
+        return np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
+
+    @property
+    def A1M(self):
+        A = np.array(
+            [
+                [
+                    -6.776660281601378126e-17,
+                    -4.069248629204854639e-01,
+                    2.905744659368497129e-01,
+                    -1.897464878848385925e-15,
+                    -3.334542096204833328e00,
+                    -1.329837852169367229e00,
+                ],
+                [
+                    -3.524072687206405985e-01,
+                    2.034624314602428152e-01,
+                    2.905744659368497129e-01,
+                    2.887798165301998399e00,
+                    -1.667271048102417330e00,
+                    1.329837852169366563e00,
+                ],
+                [
+                    3.524072687206407650e-01,
+                    2.034624314602425932e-01,
+                    2.905744659368496019e-01,
+                    2.887798165302000175e00,
+                    1.667271048102416220e00,
+                    -1.329837852169366341e00,
+                ],
+                [
+                    -1.107312451872372199e-16,
+                    -4.069248629204853529e-01,
+                    2.905744659368496574e-01,
+                    4.429249807489488797e-16,
+                    3.334542096204833328e00,
+                    1.329837852169366563e00,
+                ],
+                [
+                    -3.524072687206407095e-01,
+                    2.034624314602426765e-01,
+                    2.905744659368496019e-01,
+                    -2.887798165301998843e00,
+                    1.667271048102417108e00,
+                    -1.329837852169367229e00,
+                ],
+                [
+                    3.524072687206407650e-01,
+                    2.034624314602426765e-01,
+                    2.905744659368496019e-01,
+                    -2.887798165301999287e00,
+                    -1.667271048102416220e00,
+                    1.329837852169366785e00,
+                ],
+            ]
+        )
+        A1 = A / M
+        A1M = A1[3:, :]
+
+        return A1M
+
+    def get_initial_gesses(self):
+        """
+        Get the initial guesses for the parameters.
+
+        Output:
+        - initial_params: The initial guesses for the parameters.
+        """
+        c = np.zeros((3, 1), dtype=float)
+        initial_params_list = [self.L, c, self.A1M]
+        initial_params = np.concatenate(
+            [param.flatten() for param in initial_params_list]
+        )
+
+        L = self.L
+        L = np.array(
+            [
+                [L[0], 0, 0],
+                [L[1], L[2], 0],
+                [L[3], L[4], L[5]],
+            ]
+        )
+
+        print("Initial L:\n", L)
+        print("\nInitial c:\n", c)
+        print("\nInitial A1M:\n", self.A1M)
+        print("- - - - - - - - - - - - - - - - - - - - - -\n")
+
+        return initial_params
+
     def read_file(self, file_name):
+        """
+        Read the input file and store the data in the class variables.
+
+        Parameters:
+        - file_name: The name of the input file.
+        """
         with open(file_name) as fp:
             contents = fp.read()
             measurements = contents.split("\n\n")
@@ -118,130 +226,85 @@ class Estimator:
                 self.R.append(self.get_R(m[2]))
                 self.u.append(self.parse_line(m[3]).T)
 
-    def get_S(self, v):
-        """
-                |   0    -v3     v2 |
-        S(v) =  |  v3      0    -v1 |
-                | -v2     v1      0 |
-        """
-        v = v.flatten()
-        return np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
+    def peprocess_data(self):
+        """Pecumpute the data to be used in the cost function."""
+
+        self.w_diff = [
+            ((self.w[i + 1] - self.w[i]) / self.timestamp[i])
+            for i in range(len(self.timestamp) - 1)
+        ]
+        self.w_avg = [(self.w[i + 1] + self.w[i]) / 2 for i in range(len(self.w) - 1)]
+        self.u_avg = [(self.u[i + 1] + self.u[i]) / 2 for i in range(len(self.u) - 1)]
+
+        R_svd = [np.linalg.svd(self.R[i + 1]) for i in range(len(self.R) - 1)]
+        self.R_avg = [
+            R_svd[i][0]
+            @ np.diag([1, 1, np.linalg.det(R_svd[i][0]) * np.linalg.det(R_svd[i][2])])
+            @ R_svd[i][2]
+            for i in range(len(R_svd))
+        ]
 
     def cost_function(self, params):
+        """
+        Cost function to minimize.
+
+        Parameters:
+        - params: The parameters to optimize.
+
+        Output:
+        - residuals: The residuals of the cost function.
+        """
         residuals = []
         L_flat, c, A1M_flat = np.split(params, [6, 9])
         L = np.array(
-            [[0, 0, 0], [L_flat[0], L_flat[1], 0], [L_flat[3], L_flat[4], L_flat[5]]]
+            [
+                [L_flat[0], 0, 0],
+                [L_flat[1], L_flat[2], 0],
+                [L_flat[3], L_flat[4], L_flat[5]],
+            ]
         )
 
         A1M = A1M_flat.reshape((3, 6))
 
         for i in range(len(self.timestamp) - 1):
-            w_diff = (self.w[i + 1] - self.w[i]) / self.timestamp[i]
-            w_avg = (self.w[i + 1] + self.w[i]) / 2
-            u_avg = (self.u[i + 1] + self.u[i]) / 2
-            R_svd = np.linalg.svd(self.R[i + 1])
-            R_avg = (
-                R_svd[0]
-                @ np.diag([1, 1, np.linalg.det(R_svd[0]) * np.linalg.det(R_svd[2])])
-                @ R_svd[2]
-            )
-
             model = (
-                L @ L.T @ w_diff
-                + self.get_S(w_avg) @ L @ L.T @ w_avg
-                - self.get_S(c) @ R_avg.T @ g
-                - A1M @ u_avg
+                L @ L.T @ self.w_diff[i]
+                + self.get_S(self.w_avg[i]) @ L @ L.T @ self.w_avg[i]
+                - self.get_S(c) @ self.R_avg[i].T @ g
+                - A1M @ self.u_avg[i]
             )
 
             residuals.append(model.flatten())
 
         return np.concatenate(residuals)
 
+    def solve(self):
+        """Solve the optimization problem."""
+
+        result = sopt.least_squares(
+            self.cost_function, self.get_initial_gesses(), verbose=2
+        )
+
+        optimized_L = result.x[:6]
+        optimized_L = np.array(
+            [
+                [optimized_L[0], 0, 0],
+                [optimized_L[1], optimized_L[2], 0],
+                [optimized_L[3], optimized_L[4], optimized_L[5]],
+            ]
+        )
+        optimized_c = result.x[6:9].reshape((3, 1))
+        optimized_A1M = result.x[9:].reshape((3, 6))
+
+        print("\n- - - - - - - - - - - - - - - - - - - - - -")
+        print("Optimized L:\n", optimized_L)
+        print("\nOptimized c:\n", optimized_c)
+        print("\nOptimized A1M:\n", optimized_A1M)
+
 
 def main():
     est = Estimator()
-
-    # Initial guesses
-    L = est.get_L()
-    c = np.zeros((3, 1), dtype=float)
-    A = np.array(
-        [
-            [
-                -6.776660281601378126e-17,
-                -4.069248629204854639e-01,
-                2.905744659368497129e-01,
-                -1.897464878848385925e-15,
-                -3.334542096204833328e00,
-                -1.329837852169367229e00,
-            ],
-            [
-                -3.524072687206405985e-01,
-                2.034624314602428152e-01,
-                2.905744659368497129e-01,
-                2.887798165301998399e00,
-                -1.667271048102417330e00,
-                1.329837852169366563e00,
-            ],
-            [
-                3.524072687206407650e-01,
-                2.034624314602425932e-01,
-                2.905744659368496019e-01,
-                2.887798165302000175e00,
-                1.667271048102416220e00,
-                -1.329837852169366341e00,
-            ],
-            [
-                -1.107312451872372199e-16,
-                -4.069248629204853529e-01,
-                2.905744659368496574e-01,
-                4.429249807489488797e-16,
-                3.334542096204833328e00,
-                1.329837852169366563e00,
-            ],
-            [
-                -3.524072687206407095e-01,
-                2.034624314602426765e-01,
-                2.905744659368496019e-01,
-                -2.887798165301998843e00,
-                1.667271048102417108e00,
-                -1.329837852169367229e00,
-            ],
-            [
-                3.524072687206407650e-01,
-                2.034624314602426765e-01,
-                2.905744659368496019e-01,
-                -2.887798165301999287e00,
-                -1.667271048102416220e00,
-                1.329837852169366785e00,
-            ],
-        ]
-    )
-    A1 = A / M
-    A1M = A1[3:, :]
-
-    initial_params_list = [L, c, A1M]
-    initial_params = np.concatenate([param.flatten() for param in initial_params_list])
-
-    result = sopt.least_squares(est.cost_function, initial_params, verbose=2)
-
-    optimized_L = result.x[:6]
-    optimized_L = np.array(
-        [
-            [optimized_L[0], 0, 0],
-            [optimized_L[1], optimized_L[2], 0],
-            [optimized_L[3], optimized_L[4], optimized_L[5]],
-        ]
-    )
-    optimized_c = result.x[6:9].reshape((3, 1))
-    optimized_A1M = result.x[9:].reshape((3, 6))
-
-    print("Optimized L:")
-    print(optimized_L)
-    print("\nOptimized c:")
-    print(optimized_c)
-    print("\nOptimized A1M:")
-    print(optimized_A1M)
+    est.solve()
 
 
 if __name__ == "__main__":
