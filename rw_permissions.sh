2e8628c87f78a0854971c0eed0dd1ccc87e72c94
Diogo Ferreira <itsnotsoftware@gmail.com>
2025-03-25 16:55:50 +0000

    cleanup

--- rw_permissions.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# simple script to give ssh users read and write permissions
-sudo chmod -R a+rw /home/nvidia/Desktop/software_cobot
--- slam_dev_sandbox/2d/utils/odom_pose.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class OdomPose:
-    x: float
-    y: float
-    theta: float
